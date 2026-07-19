f595249b0a3de9554886514c45c8722a1182b255
Games58152 <games058152@gmail.com>
2026-07-20 00:54:14 +0530

    update

--- automation_exercise/utils/driver.py
+++ automation_exercise/utils/driver.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-
+from selenium.webdriver.chrome.service import Service
 def get_driver():
     service = Service(r"C:\Drivers\chromedriver.exe")
     driver = webdriver.Chrome(service=service)
