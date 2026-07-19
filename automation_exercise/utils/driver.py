f67512bd6f7d1856201bff13b26cfc0ed1b9c551
Games58152 <games058152@gmail.com>
2026-07-20 00:52:12 +0530

    update

--- automation_exercise/utils/driver.py
+++ automation_exercise/utils/driver.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 
 def get_driver():
-    driver=webdriver.Chrome()
+    service = Service(r"C:\Drivers\chromedriver.exe")
+    driver = webdriver.Chrome(service=service)
+    
     driver.maximize_window()
     driver.implicitly_wait(10)
     return driver
